12bfbff618cc5163d956ec02cdd34d35b13c944d
PavanKumar1174 <andepavan37@gmail.com>
2024-09-02 19:34:20 +0530

    last commit

--- src/Fagito/API_Integration/data.jsx
+++ src/Fagito/API_Integration/data.jsx
@@ -1,3 +1,4 @@
-export const API_URl="https://swiggy-clone-backend-1.onrender.com"
+export const API_URl="https://swiggy-clone-backend-6pav.onrender.com"
 
  //export const API_URl="http://localhost:5000"
+ //https://swiggy-clone-backend-1.onrender.com
